d69ea0ef2ce4cc770a6c26f4f4f53d471a789f44
Nguyên Anh <nngguyen.anh@gmail.com>
2022-05-07 07:46:21 +0700

    update

--- src/Command/User/commands/work.ts
+++ src/Command/User/commands/work.ts
@@ -1,9 +1,9 @@
-import {inlineCode} from '@discordjs/builders'
+import { inlineCode } from '@discordjs/builders'
 import currency from 'currency.js'
-import {Message} from 'discord.js'
+import { Message } from 'discord.js'
 import formatDuration from 'format-duration'
 import message from 'Handler/message'
-import {UserCommand} from '.'
+import { UserCommand } from '.'
 import User from '..'
 import Constant from '../../../Constant'
 
@@ -15,13 +15,13 @@ export default class Work extends UserCommand {
         this._name = this._name + ' work'
         this._alias = this._alias + ' w'
     }
-    createQuestion():{question:string,answer: string} {
-        let x = Math.round(Math.random() * 20 + 1)
-        let y = Math.round(Math.random() * x + 1)
-        const operatorList = ['+', '-', 'x', '/']
-        const operatorRandom = Math.round(Math.random() * operatorList.length)
+    createQuestion(): { question: string, answer: string } {
+        let x = Math.floor(Math.random() * 20 + 1)
+        let y = Math.floor(Math.random() * x + 1)
+        const operatorList = ['+', '-', 'x']
+        const operatorRandom = Math.floor(Math.random() * operatorList.length)
         const question = `Answer the question: ${x} ${operatorList[operatorRandom]} ${y} = ?`
-        let answer 
+        let answer
         switch (operatorRandom) {
             case 0:
                 answer = x + y
@@ -30,21 +30,15 @@ export default class Work extends UserCommand {
                 answer = x - y
                 break;
             case 2:
+                x = Math.floor(Math.random() * 30 + 1)
+                y = Math.floor(Math.random() * x + 1)
                 answer = x * y
-                break;
-            case 3:
-                x = Math.round(Math.random() * 200 + 1)
-                y = Math.round(Math.random() * x + 1)
-                while (x % y != 0) {
-                    y = Math.round(Math.random() * x + 1)
-                }
-                answer = x / y
                 break;
             default:
                 answer = x + y
                 break;
         }
-        return {question,answer: answer.toString()}
+        return { question, answer: answer.toString() }
     }
 
     async execute(messageHandler: message, message: Message<boolean>) {
@@ -61,10 +55,10 @@ export default class Work extends UserCommand {
             if (duration < this._limitTime) {
                 message.reply(
                     'You must relax ' +
-                        inlineCode(
-                            formatDuration(this._limitTime - duration)
-                        ) +
-                        ' before working'
+                    inlineCode(
+                        formatDuration(this._limitTime - duration)
+                    ) +
+                    ' before working'
                 )
                 return
             }
@@ -97,7 +91,7 @@ export default class Work extends UserCommand {
                         inlineCode(currency(this._amount).format()) +
                         ' is your wage'
                     )
-                } 
+                }
                 else {
                     await this._userManager.updateUser(
                         message.author,
@@ -108,7 +102,7 @@ export default class Work extends UserCommand {
                     )
                     message.reply('Your answer isn\'t correct. Try after ' + inlineCode(formatDuration(this._limitTime)))
                 }
-            }).on('end',(collected)=>{})
+            }).on('end', (collected) => { })
 
         } catch (e) {
             message.reply('Please try again !')
